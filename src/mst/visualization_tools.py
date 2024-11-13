d4c6031ee049e5b94afab68c72069950e2b0e4a1
Lukas Retschmeier <lure@di.ku.dk>
2024-11-13 11:18:20 +0100

    Cleaned up second experiment as well
    Signed-off-by: Lukas Retschmeier <lure@di.ku.dk>

--- src/mst/visualization_tools.py
+++ src/mst/visualization_tools.py
@@ -3,51 +3,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def init_plot(results, rho_values, title):
+def init_plot(results, rho_values, meta_params):
     """
     Requires a plt.show() later.
     :param results:
     :param rho_values:
     :return:
     """
-    fig, axs = plt.subplots(1, 2, figsize=(10, 5))  # 2x2 grid
-    fig.suptitle(title)
+    fig, ax= plt.subplots(figsize=(10, 10))  # 2x2 grid
+    # fig = plt.plot( figsize=(10, 5))  # 2x2 grid
+    ax.set_title(meta_params["title"])
     (sealfon, our, pamst, real) = (results['sealfon'], results['our'], results['pamst'], results['real'])
-    # Error of the MST
-    sns.lineplot(x=rho_values, y=sealfon, marker='o', label="Sealfon", ax=axs[0])
-    # sns.lineplot(x=rho_values, y=upper, marker='o', label="upperbound", ax=axs[0])
-    sns.lineplot(x=rho_values, y=our, marker='o', label="$\\textbf{Our}$", ax=axs[0])
-    sns.lineplot(x=rho_values, y=pamst, marker='o', label="PAMST", ax=axs[0])
-    sns.lineplot(x=rho_values, y=real, marker='o', label="Real MST", ax=axs[0])
-    axs[0].set_title("Absolute Weight of the MST")
-    # Error of the MST
     sns.lineplot(x=rho_values, y=[sealfon - real for sealfon, real in zip(sealfon, real)], marker='o', label="Sealfon",
-                 ax=axs[1])
+                 ax=ax)
     sns.lineplot(x=rho_values, y=[ours - real for ours, real in zip(our, real)], marker='o', label="$\\textbf{Our}$",
-                 ax=axs[1])
+                ax=ax)
     sns.lineplot(x=rho_values, y=[pamst - real for pamst, real in zip(results['pamst'], real)], marker='o',
-                 label="PAMST", ax=axs[1])
-
-    axs[1].set_title("Additive Error of the MST")
-    axs[0].set_ylabel("Absolute Weight")
-    # axs[0].set_ylim([0,100])
-
-    axs[0].set_xlabel("$\\rho$")
-    axs[1].set_ylabel("Additive Error")
-
-    plt.legend(title="Comparing Private MST Algs")
+                 label="PAMST", ax=ax)
+    ax.set_ylabel("Additive Error")
+    # plt.legend(title="Comparing Private MST ")
+   # plt.subplots_adjust(hspace = 0.4)
 
 def init_multiplot(all_results, rho_values, meta_params, columns=2):
-
     number_of_rows = max(1,
                          int(
-                             np.ceil(len(all_results)) / columns)
-    #np.ceil(min(columns, len(all_results)) / columns)
-                         ) + 1
+                             np.ceil(len(all_results)) / columns)) + 1
     fig, axs = plt.subplots(number_of_rows, columns, figsize=(20, 17))  # Grid layout
     fig.suptitle(meta_params["title"])
 
-    # Turn off all axes by default
+    # we only want to see used axes
     for ax in axs.flat:
         ax.axis('off')
 
@@ -74,7 +58,7 @@ def init_multiplot(all_results, rho_values, meta_params, columns=2):
         # axs[0].set_ylim([0,100])
         # axs[index].set_ylabel("Additive Error")
 
-#     plt.legend(title="Comparing Private MST Algs")
+    plt.subplots_adjust(hspace = 0.4)
     return (fig, axs)
 
 
--- src/util/ioutils.py
+++ src/util/ioutils.py
@@ -1,14 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 def encode_experiment_one(results, edge_probabilites, rho_values, n, maximum_edge_weight, sensitivity):
-    return {
-        "results": results,
-        "rho_values": rho_values,
-        "edge_probabilities": edge_probabilites,
-        "n": n,
-        "maximum_edge_weight": maximum_edge_weight,
-        "sensitivity": sensitivity
-    }
+    return
 def save_results(results, filename="save/test.npy"):
     """
     Takes a dictionary and saves all entries to a file.
@@ -19,11 +12,33 @@ def save_results(results, filename="save/test.npy"):
     with open(filename, 'wb') as f:
         np.savez(f, **results)
 
-def save_plot_and_data(results_complete, edge_probabilities, rho_values, n, maximum_edge_weight, sensitivity):
-    file_name = "save/complete_n{}_sens{}_range0-{}x".format(n, sensitivity, maximum_edge_weight)
-    save_results(filename=file_name + ".npy", results=encode_experiment_one(
-        results_complete, edge_probabilities, rho_values,n, maximum_edge_weight, sensitivity))
+def save_plot_and_data_experiment_one(results_complete, edge_probabilities, rho_values, n, maximum_edge_weight, sensitivity):
+    file_name = "save/complete_n{}_sens{}_range0-{}".format(n, sensitivity, maximum_edge_weight)
+    save_results(filename=file_name + ".npy", results=
+    {
+        "results": results_complete,
+        "rho_values": rho_values,
+        "edge_probabilities": edge_probabilities,
+        "n": n,
+        "maximum_edge_weight": maximum_edge_weight,
+        "sensitivity": sensitivity
+    })
     plt.savefig(file_name + ".png")
+    plt.savefig(file_name + ".pdf", format='pdf', bbox_inches='tight')
+
+def save_plot_and_data_experiment_two(results_complete, rho_values, n, sensitivity):
+    file_name = "save/mutual-information_n{}_sens{}".format(n, sensitivity)
+    save_results(filename=file_name + ".npy", results=
+    {
+        "results": results_complete,
+        "rho_values": rho_values,
+        "n": n,
+        "sensitivity": sensitivity
+    })
+    plt.savefig(file_name + ".png")
+    plt.savefig(file_name + ".pdf", format='pdf', bbox_inches='tight')
+
+
 
 
 def load_results(filename):
